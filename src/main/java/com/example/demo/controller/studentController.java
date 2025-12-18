30d8c664344b89e29afe84fa8b6945320f480e2d
bala-amypo <bala@amypo.in>
2025-12-18 08:03:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/studentController.java
+++ src/main/java/com/example/demo/controller/studentController.java
@@ -13,7 +13,7 @@ import com.example.demo.service.*;
 public class studentController {
 
     @Autowired
-    studentService service;
+    StudentService service;
 
     @GetMapping("/getAllStudent")
     public List<studentEntity> getAll(){
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.entity.studentEntity;
 
-public interface studentService {
+public interface StudentService {
 
     List<studentEntity> getAllStudents();
 
